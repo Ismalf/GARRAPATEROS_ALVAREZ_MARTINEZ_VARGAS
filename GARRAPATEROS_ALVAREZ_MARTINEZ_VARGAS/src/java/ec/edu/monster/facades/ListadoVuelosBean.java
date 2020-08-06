dc36f23e8ffe222a2a5aeec89dcaf36924601a2d
sebalv96 <ffsebastian96@gmail.com>
2020-08-06 00:40:02 -0500

    Actualización datatable

---
+++ GARRAPATEROS_ALVAREZ_MARTINEZ_VARGAS/src/java/ec/edu/monster/facades/ListadoVuelosBean.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.monster.facades;
+
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author sebas
+ */
+@Named(value = "listadoVuelosBean")
+@RequestScoped
+public class ListadoVuelosBean {
+
+    /**
+     * Creates a new instance of ListadoVuelosBean
+     */
+    @EJB
+    private VuelosFacade vueFacade;
+    private List<Object[]> listado;
+
+    public List<Object[]> getListado() {
+        return listado;
+    }
+
+    public void setListado(List<Object[]> listado) {
+        this.listado = listado;
+    }
+    
+    
+    
+    
+    /**
+     * Creates a new instance of ListadoVuelosBean
+     */
+    public ListadoVuelosBean() {
+    }
+    
+    @PostConstruct
+    public void llenarTabla()
+    {
+        //System.out.println("VALORRRRRRRRRR: " +listado.get(0));
+        listado=vueFacade.obtenerInfoVuelos();
+        
+    }
+    
+}
--- GARRAPATEROS_ALVAREZ_MARTINEZ_VARGAS/src/java/ec/edu/monster/facades/VuelosFacade.java
+++ GARRAPATEROS_ALVAREZ_MARTINEZ_VARGAS/src/java/ec/edu/monster/facades/VuelosFacade.java
@@ -6,9 +6,11 @@
 package ec.edu.monster.facades;
 
 import ec.edu.monster.modelo.Vuelos;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -17,7 +19,7 @@ import javax.persistence.PersistenceContext;
 @Stateless
 public class VuelosFacade extends AbstractFacade<Vuelos> {
 
-    @PersistenceContext(unitName = "GARRAPATEROS_ALVAREZ_MARTINEZ_VARGASPU")
+        @PersistenceContext(unitName = "GARRAPATEROS_ALVAREZ_MARTINEZ_VARGASPU")
     private EntityManager em;
 
     @Override
@@ -27,6 +29,16 @@ public class VuelosFacade extends AbstractFacade<Vuelos> {
 
     public VuelosFacade() {
         super(Vuelos.class);
+    }
+    public List obtenerInfoVuelos()
+    {
+       Query q= em.createNativeQuery("SELECT V.ID_VUELO,V.ID_AVIONETA, concat_ws(' ', P.PEPER_NOMBRE,P.PEPER_APELLI) as Nombre,V.HORA_SALIDA,V.HORA_LLEGADA,V.FECHA_VUELO\n" 
+                                        +"FROM vuelos V\n" 
+                                        +"INNER JOIN  PEPER_PERSON P\n" 
+                                        +"ON V.PEPER_ID=P.PEPER_ID");
+       // Query q=em.createNativeQuery("select * from vuelos");
+        List<Object[]> listado=q.getResultList();
+        return listado;
     }
     
 }
